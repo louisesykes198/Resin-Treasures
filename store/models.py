1d0f6a1bc3169711b948467200c602c41b1002e9
louisesykes198 <louisesykes198@yahoo.com>
2025-08-01 15:21:10 +0100

    Added a shop.html files, superuser with categories and product header

--- store/models.py
+++ store/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name_plural = "Categories"  
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='products/')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
--- store/views.py
+++ store/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
-
+from .models import Product, Category
 
 def home(request):
     return render(request, 'store/home.html')
+
+def about(request):
+    return render(request, 'store/about.html')
+
+def contact(request):
+    return render(request, 'store/contact.html')
+
+def shop(request):
+    products = Product.objects.all()
+    categories = Category.objects.all()
+    return render(request, 'store/shop.html', {'products': products, 'categories': categories})
