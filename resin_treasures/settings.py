97ee93f588b3d1900640acd00f2ebd3b6c5beae7
louisesykes198 <louisesykes198@yahoo.com>
2025-08-12 11:50:15 +0100

    Fix contact form email sending: deliver emails to inbox, not sent box; update SMTP settings for resin email

--- resin_treasures/settings.py
+++ resin_treasures/settings.py
@@ -104,7 +104,14 @@ DATABASES = {
 # If DATABASE_URL is set (e.g., on Heroku), override the default database settings
 if os.environ.get('DATABASE_URL'):
     DATABASES['default'] = dj_database_url.config(conn_max_age=600)
-
+    
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'resintreasures5@gmail.com' 
+EMAIL_HOST_PASSWORD = 'affqlcsupocdyzpg'
+DEFAULT_FROM_EMAIL = 'resintreasures5@gmail.com'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
