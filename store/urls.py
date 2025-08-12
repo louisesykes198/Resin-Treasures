b8da0b46a8231e1ea191d9870555fabfb26a3813
louisesykes198 <louisesykes198@yahoo.com>
2025-08-12 15:53:31 +0100

    Added the wishlish.html, css and a floating button

--- store/urls.py
+++ store/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'),
     path('shop/', views.shop, name='shop'),
     path('product/<int:pk>/', views.product_detail, name='product_detail'), 
+    path('wishlist/', views.wishlist_view, name='wishlist'),
+    path('wishlist/add/<int:product_id>/', views.add_to_wishlist, name='add_to_wishlist'),
+    path('wishlist/remove/<int:product_id>/', views.remove_from_wishlist, name='remove_from_wishlist'),
 ]
