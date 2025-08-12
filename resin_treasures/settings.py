ddfe2b1b46389e372620ce28570346c6754522cb
louisesykes198 <louisesykes198@yahoo.com>
2025-08-12 12:42:35 +0100

    Removed sensitive info from my repo

--- resin_treasures/settings.py
+++ resin_treasures/settings.py
@@ -109,8 +109,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'resintreasures5@gmail.com' 
-EMAIL_HOST_PASSWORD = 'affqlcsupocdyzpg'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = 'resintreasures5@gmail.com'
 
 # Password validation
