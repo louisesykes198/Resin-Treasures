e7187f9fe5e1fb19f9e0e372d4e7a7d1d672c508
louisesykes198 <louisesykes198@yahoo.com>
2025-08-13 13:31:13 +0100

    Added a setting.html to the my account dropdown

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,15 +1,13 @@
-from django.contrib.auth import views as auth_views
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 from .views import CustomLogoutView
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path(
-        'login/',
-        auth_views.LoginView.as_view(template_name='accounts/login.html'),
-        name='login'
-    ),
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('my-account/', views.my_account, name='my_account'),
     path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path('account/settings/', views.account_settings, name='account_settings'),
 ]
+
