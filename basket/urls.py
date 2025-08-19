91ad33c20386e6dbcdb99f9122bbbda99ee39a1f
louisesykes198 <louisesykes198@yahoo.com>
2025-08-19 10:11:45 +0100

    Added a bin icon to the basket to remove one item

--- basket/urls.py
+++ basket/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.basket_summary, name='basket_summary'),
     path('add/<int:product_id>/', views.add_to_basket, name='add_to_basket'),
     path('remove/<int:product_id>/', views.remove_from_basket, name='remove_from_basket'),
     path('empty/', views.empty_basket, name='empty_basket'),
+    path('', views.basket_summary, name='basket_summary'),
 ]
+
+
 
 
 
