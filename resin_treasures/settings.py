19f988233b924460b9e7cba0c17da48d8a7843a9
louisesykes198 <louisesykes198@yahoo.com>
2025-08-28 15:09:52 +0100

    Added Am I Responsive images for readme file

--- resin_treasures/settings.py
+++ resin_treasures/settings.py
@@ -73,7 +73,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
